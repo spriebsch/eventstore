d5c478099a3f2544b6a6da59286eef125c808883
Stefan Priebsch <stefan@priebsch.de>
2024-03-07 05:28:36 +0100

    Use AUTOINCREMENT.

--- src/schema/SqliteEventStoreSchema.php
+++ src/schema/SqliteEventStoreSchema.php
@@ -39,7 +39,7 @@ final class SqliteEventStoreSchema extends SqliteSchema
     private function sql(): string
     {
         return 'CREATE TABLE `events` (
-            `id` INTEGER PRIMARY KEY,
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
             `eventId` TEXT UNIQUE,
             `correlationId` TEXT,
             `topic` TEXT,
